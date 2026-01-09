48ef0e2e0ac3d0fc7cb973ce7b12250bda4a7af5
yuval <yuval@lenovo.2020>
2026-01-10 01:50:05 +0200

    medians_fast

--- code/rain_update.py
+++ code/rain_update.py
@@ -64,7 +64,30 @@ print('saving rain update')
 df_rain.to_csv(opcsv, index=False)
 print('rounding rain data')
 round_data(opcsv)
-
+current_year = datetime.now().year
+winters = pd.read_csv('data/sum_rain_sep_to_aug.csv')
+for year in [current_year-1, current_year]:
+    df = pd.read_csv(f'data/rain_{year}.csv')
+    # half2: Jan 1 to Sept 1 of current year
+    df2 = df[df['datetime'] < f'{year}-09-01']
+    # half2 = np.nansum(df[station][df['datetime'] < f'{year}-09-01'])
+    
+    if year == current_year-1:
+        df_prev = df
+        continue
+    # half1: Sept 1 of previous year to Jan 1 of current year
+    df1 = df_prev[df_prev['datetime'] >= f'{year-1}-09-01']
+    df_combined = pd.concat([df1, df2])
+    idate = np.where(np.array([md[5:10] for md in df_combined['datetime']]) == '01-03')[0][-1]
+    month_day_okay = np.array([False]*len(df_combined))
+    month_day_okay[:idate+1] = True
+    df_combined = df_combined[month_day_okay]
+    for station in df_combined.columns[1:]:
+        total = np.nansum(df_combined[station])
+        winters.at[len(winters)-1, station] = total
+    df_prev = df
+winters.to_csv('data/sum_rain_sep_to_aug.csv', index=False)
+round_data('data/sum_rain_sep_to_aug.csv')
 '''TODO: 
 - try complete past data
 - make winter by winter table, update web page
