64ba5afb135ba2b11fd7acc7757bf866745aa04a
yuval <yuval@lenovo.2020>
2026-01-31 23:17:41 +0200

    twice

--- code/scrap.py
+++ code/scrap.py
@@ -6,25 +6,18 @@ sys.path.append(os.environ['HOME']+'/weather/code')
 from weather import *
 import numpy as np
 from glob import glob
-data = query_temp(station='EZUZ', from_date='2016-02-22', to_date='2016-02-22', monitor='TDmin')
-for ii in range(len(data['data'])):
-    value = data['data'][ii]['channels'][0]['value']
-    if value < 0:
-        print(data['data'][ii]['datetime'], 'value: ', value, 'valid: ', data['data'][ii]['channels'][0]['valid'])
-# update_activity(ignore_old=True)
-
-# data = rain_1h(stations=['JERUSALEM GIVAT RAM', 'JERUSALEM CENTRE', 'JERUSALEM GIVAT RAM_1m', 'JERUSALEM CENTRE_1m'], from_date='2025-09-01', to_date='2026-01-19', save_csv='~/Documents/jlm.csv')
-# print(np.nansum(data.values[:, 1:], axis=0))
-# for year in range(2015, 2026):
-#     # df = pd.read_csv(f'data/rain_{year}.csv')
-#     # imax = np.nanargmax(df.values[:, 1:], axis=1)
-#     # print(f'max rain {year}: {df.columns[imax+1]} {np.max(df.values[:, imax+1])}')
-#     df = pd.read_csv(f'data/temp_min_{year}.csv')
-#     y = np.nanmin(df.values[:, 1:], axis=0)
-#     imin = np.nanargmin(y)
-#     print(f'min temp {year}: {df.columns[imin+1]} {y[imin]}')
-#     df = pd.read_csv(f'data/temp_max_{year}.csv')
-#     y = np.nanmax(df.values[:, 1:], axis=0)
-#     imax = np.nanargmax(y)
-#     print(f'max temp {year}: {df.columns[imax+1]} {y[imax]}')
+from time import sleep
+next_week = (datetime.now()+timedelta(days=7)).strftime('%Y-%m-%d')
+yesterday = (datetime.now()-timedelta(days=1)).strftime('%Y-%m-%d')
+station = 'EZUZ'
+data = query_rain(station=station, from_date='2026-01-29', to_date=next_week)
+current = data[-1]['datetime']
+for test in range(25*6):  # run a test every 10 minutes for 25 hours
     
+    print('sampling data at '+datetime.now().strftime('%H:%M:%S'))
+    data = query_temp(station=station, from_date='2026-01-29', to_date=next_week, monitor='TDmin')
+    if data[-1]['datetime'] > current:
+        current = data[-1]['datetime']
+        print(f'new data found at {current}')
+    sleep(60*10)
+print(f"last check finished at {datetime.now().strftime('%H:%M:%S')}")
