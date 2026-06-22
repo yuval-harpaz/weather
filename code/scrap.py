19ff4c14ec4da63ec579748b7e4ea19f9e7eb9b1
yuval <yuval@lenovo.2020>
2026-06-22 12:28:13 +0300

    musa

--- code/scrap.py
+++ code/scrap.py
@@ -1,36 +1,10 @@
 import os
+os.chdir(os.environ['HOME']+'/weather')
 import pandas as pd
 import sys
 from datetime import datetime
 sys.path.append(os.environ['HOME']+'/weather/code')
-from weather import *
+from weather import rain_1h, update_stations, update_activity, round_data
 import numpy as np
-from glob import glob
-from time import sleep
-import json
-
-from collect_predictions import main
-main()
-# update_stations(force=True)
-# update_regions(force=True)
-# df = pd.read_csv('data/ims_regions.csv') 
-# for iregion in range(len(df)):
-#     region = str(df['name'][iregion])
-#     stations = eval(df['stations'][iregion])
-#     stations = [s['name'] for s in stations]
-#     print(f'Region: {iregion} {region}')
-#     print('stations: '+', '.join(sorted(stations)))
-
-# df0 = pd.read_csv('data/rain_2020.csv')
-# df1 = pd.read_csv('data/rain_2021.csv')
-# df0 = df0[df0['datetime'] >= '2020-09-01']
-# df1 = df1[df1['datetime'] < '2021-09-01']
-# df = pd.concat([df0, df1])
-# stations = pd.read_csv('data/ims_stations.csv')
-# stations = np.sort(stations[stations['regionId'] == 7]['name'].tolist())
-# # winter = pd.DataFrame(columns=[])
-# for station in stations:
-#     if station not in df.columns:
-#         continue
-#     print(f"{station}: {np.nansum(df[station])}")
-    
+update_stations()
+update_activity(new=True, ignore_old=False)
